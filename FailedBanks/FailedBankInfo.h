e5f502f49ce068ab3a5a44ed7b439215f0604ed2
joshmotley <joshkmotley@gmail.com>
2015-10-04 23:19:51 -0400

    add data class

---
+++ FailedBanks/FailedBankInfo.h
@@ -0,0 +1,13 @@
+//
+//  FailedBankInfo.h
+//  FailedBanks
+//
+//  Created by Joshua Motley on 11/15/15.
+//  Copyright © 2015 Motley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FailedBankInfo : NSObject
+
+@end
---
+++ FailedBanks/FailedBankInfo.m
@@ -0,0 +1,13 @@
+//
+//  FailedBankInfo.m
+//  FailedBanks
+//
+//  Created by Joshua Motley on 11/15/15.
+//  Copyright © 2015 Motley. All rights reserved.
+//
+
+#import "FailedBankInfo.h"
+
+@implementation FailedBankInfo
+
+@end
